e84b6c6dd36a002e1a775b7d23c959b1e7358349
Barbara Gichohi <my.github.barbaragichohi77@gmail.com>
2022-08-17 20:33:15 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Code your solution in this file!
+
+
+function distanceFromHqInBlocks (value) {
+    if (value >42) {
+        return value-42;
+    }else{
+        return 42-value;
+    }
+}
+
+
+function distanceFromHqInFeet(value) {
+    return distanceFromHqInBlocks(value)*264
+}
+
+
+function distanceTravelledInFeet (start, destination) {
+    if (start === 34) {
+        return (start - destination)* 264;
+    }
+    return (destination - start) * 264;
+}
+
+function calculatesFarePrice (start, destination) {
+    if (distanceTravelledInFeet(start, destination)<400) {
+        return 0;
+    }else if (distanceTravelledInFeet(start, destination) <=2000) {
+        return (distanceTravelledInFeet(start, destination)-400)/50;
+    }else if (distanceTravelledInFeet(start, destination) >2500){
+        return 'cannot travel that far';
+    }else {
+        return 25;
+    }
+}
+
+
+
+
+
